d54ded72f3b2a4472af02b6a1a9023a2a77c6516
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-03-13 20:56:39 +0000

    Add try except to check for a valid integer

--- game.py
+++ game.py
@@ -20,18 +20,23 @@ print(f'{player}, I\'m thinking of a number between 1 and 100')
 num = random.randint(1,100)
 count = 0
 condition = True
-while condition:
-    guess = int(input("Guess the number: "))
-    if guess < 0 or guess > 100:
-        print(f"The {guess} you entered is invalid")
-        
-    elif guess > num:
-        count = count + 1
-    elif guess < num:
-        print("Your guess was too low! Please try again")    
-        count = count + 1
-    else:
-        condition = False
-        
+
+while condition:  
+    guess = input("Guess the number: ")
+    try:
+        guess = int(guess)
+        if guess < 0 or guess > 100:
+            print(f"The {guess} you entered is invalid")             
+        elif guess > num:
+            print("Your guess was too high! Please try again")    
+            count = count + 1
+        elif guess < num:
+            print("Your guess was too low! Please try again")    
+            count = count + 1
+        else:
+            condition = False
+    except ValueError:
+        print("The number needs to be an integer")  
+
 print(f"Congratulations {player}. You guessed the number correctly")
 print(f"Number of guesses: {count}")
